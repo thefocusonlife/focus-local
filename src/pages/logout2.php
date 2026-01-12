db96589d78c97ef39e5cbb2bd1ea86c001062f32
Geoff Stevens <geoff@thefocusonlife.org>
2026-01-12 11:06:08 -0800

    Remove legacy logout2 handler after deep-link hardening

--- src/pages/logout2.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-$key = $_SESSION['website']; // Use strict types
-$cms->getSession()->delete(); // Call method to end session
-$cms->getSession()->create(0, $key); // Redirect to home page
-redirect('index/' . $key);
